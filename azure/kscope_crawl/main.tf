bcfa23dc71b01fcbdf3dcfc74b2267a7ead2b315
usmanklodhi <usmanklodhii@gmail.com>
2023-06-07 17:53:13 +0500

    combining terraform script into one file

--- azure/kscope_crawl/main.tf
+++ azure/kscope_crawl/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = ">= 2.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -45,8 +46,67 @@ resource "azuread_application" "example" {
   }
 }
 
-//print client id
-output "cient_id" {
+resource "azurerm_resource_group" "example" {
+  name     = "storage-rg"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "k6scopemystorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "k6scopemycontainer"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_queue" "example" {
+  name                 = "kscopemyqueue"
+  storage_account_name = azurerm_storage_account.example.name
+}
+
+locals {
+  resourceId = "${azurerm_storage_account.example.id}/blobServices/default"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "storage_account_logs" {
+  name               = "storage-account-logs"
+  target_resource_id = local.resourceId
+  storage_account_id = azurerm_storage_account.example.id
+
+  enabled_log {
+    category = "StorageRead"
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "StorageWrite"
+    retention_policy {
+      enabled = false
+    }
+  }
+  enabled_log {
+    category = "StorageDelete"
+    retention_policy {
+      enabled = false
+    }
+  }
+  metric {
+    category = "AllMetrics"
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+// Print client id
+output "client_id" {
   value = azuread_application.example.application_id
 }
 
@@ -68,4 +128,21 @@ output "tenant_id" {
 //print owner object id
 output "owner_object_id" {
   value = data.azurerm_client_config.current.object_id
+}
+
+output "storageAccountName" {
+  value = azurerm_storage_account.example.name
+}
+
+output "storageContainerName" {
+  value = azurerm_storage_container.example.name
+}
+
+output "storageQueueName" {
+  value = azurerm_storage_queue.example.name
+}
+
+output "storageAccountKey" {
+  value     = azurerm_storage_account.example.primary_access_key
+  sensitive = true
 }
