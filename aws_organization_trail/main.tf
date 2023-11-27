6e0f1fb74ccb73eb4031ba93a0db73ce533ecfbc
Jack Kinga <jackmwangi@gmail.com>
2023-11-27 08:30:45 +0300

    update cloudtrail and variable definitions

--- aws_organization_trail/main.tf
+++ aws_organization_trail/main.tf
@@ -18,20 +18,43 @@ resource "aws_cloudtrail" "data" {
   name = local.cloudtrail_name
 
   s3_bucket_name                = var.s3_bucket_name
+  s3_key_prefix                 = var.s3_key_prefix
   include_global_service_events = true
   is_multi_region_trail         = true
   cloud_watch_logs_group_arn    = var.logs_group_arn
   cloud_watch_logs_role_arn     = var.logs_role_arn
   enable_log_file_validation    = true
   enable_logging                = true
+  sns_topic_name                = var.sns_topic_name
+  is_organization_trail         = true
 
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = false
 
-  # enables all data events.
-  advanced_event_selector {
-    name = "Data Event Selector"
-    field_selector {
-      field  = "eventCategory"
-      equals = ["Data"]
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3"]
+    }
+  }
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = false
+
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = false
+
+    data_resource {
+      type   = "AWS::DynamoDB::Table"
+      values = ["arn:aws:dynamodb"]
     }
   }
 }
--- aws_organization_trail/variables.tf
+++ aws_organization_trail/variables.tf
@@ -19,6 +19,11 @@ variable "s3_bucket_name" {
   description = "where the trail will save it's log files. should be identical to the management trail"
 }
 
+variable "s3_key_prefix" {
+  type        = string
+  description = "S3 key prefix that follows the name of the bucket you have designated for log file delivery"
+}
+
 variable "logs_group_arn" {
   type        = string
   description = "log group to which CloudTrail logs will be delivered. should be identical to the management trail"
@@ -27,6 +32,11 @@ variable "logs_group_arn" {
 variable "logs_role_arn" {
   type        = string
   description = "Role for the CloudWatch Logs endpoint to assume to write to a user’s log group. Should be identical to the management trail"
+}
+
+variable "sns_topic_name" {
+  type        = string
+  description = "Name of the Amazon SNS topic defined for notification of log file delivery"
 }
 
 locals {
